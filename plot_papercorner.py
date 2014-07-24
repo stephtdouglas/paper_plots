d4f5a5ad955a3d9a541e505dbc8d0f31c134709c
stephtdouglas <stephanietdouglas@gmail.com>
2014-07-24 10:39:35 -0400

    change to send to Phil

--- plot_papercorner.py
+++ plot_papercorner.py
@@ -1,4 +1,5 @@
 import triangle, cPickle
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -8,12 +9,13 @@ def quantile(x,quantiles):
     return zip(quantiles,qvalues)
 
 
-infile = open('/home/stephanie/ptf/fit_rossby/fit_rossby.pkl','rb')
+#infile = open('/home/stephanie/ptf/fit_rossby/fit_rossby.pkl','rb')
+infile = open('fit_rossby.pkl','rb')
 samples = cPickle.load(infile)
 infile.close()
-sl_mcmc = quantile(samples[:,0],[.16,.5,.84])
-to_mcmc = quantile(samples[:,1],[.16,.5,.84])
-be_mcmc = quantile(samples[:,2],[.16,.5,.84])
+sl_mcmc = quantile(samples[:,0],[.16,.5,.84]) # saturation level
+to_mcmc = quantile(samples[:,1],[.16,.5,.84]) # turnover
+be_mcmc = quantile(samples[:,2],[.16,.5,.84]) # beta
 
 ndim=3
 
@@ -42,31 +44,29 @@ ax8.set_xlabel(rolabel,fontsize='x-large')
 ax9 = plt.subplot(339)
 ax9.set_xlabel(blabel,fontsize='x-large')
 
-plt.savefig('paper_corner.png')
-plt.savefig('paper_corner.ps')
+plt.savefig('paper_corner.png',dpi=300,bbox_inches='tight')
 
 
 # Add quantiles/lit values
 ax1 = plt.subplot(331)
 yl = ax1.get_ylim()
-ax1.plot((sl_mcmc[1][1],sl_mcmc[1][1]),yl,'k-')
+ax1.plot((sl_mcmc[1][1],sl_mcmc[1][1]),yl,'-',color='grey')
 ax1.plot((sl_mcmc[0][1],sl_mcmc[0][1]),yl,'-',color='grey')
 ax1.plot((sl_mcmc[2][1],sl_mcmc[2][1]),yl,'-',color='grey')
 
 
 ax5 = plt.subplot(335)
 yl = ax5.get_ylim()
-ax5.plot((to_mcmc[1][1],to_mcmc[1][1]),yl,'k-')
+ax5.plot((to_mcmc[1][1],to_mcmc[1][1]),yl,'-',color='grey')
 ax5.plot((to_mcmc[0][1],to_mcmc[0][1]),yl,'-',color='grey')
 ax5.plot((to_mcmc[2][1],to_mcmc[2][1]),yl,'-',color='grey')
 
 
 yl = ax9.get_ylim()
-ax9.plot((be_mcmc[1][1],be_mcmc[1][1]),yl,'k-')
+ax9.plot((be_mcmc[1][1],be_mcmc[1][1]),yl,'-',color='grey')
 ax9.plot((be_mcmc[0][1],be_mcmc[0][1]),yl,'-',color='grey')
 ax9.plot((be_mcmc[2][1],be_mcmc[2][1]),yl,'-',color='grey')
-ax9.plot((-1,-1),yl,'k--')
+#ax9.plot((-1,-1),yl,'k--') # Jackson & Jeffries
 
-plt.savefig('paper_corner_quantiles.png')
-plt.savefig('paper_corner_quantiles.ps')
+plt.savefig('paper_corner_quantiles.png',dpi=300,bbox_inches='tight')
 
